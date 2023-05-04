86fd3ab834460a379ce6ddc4f3d4d4b63f98cf8b
Yuvraj <128832039+yuvgit@users.noreply.github.com>
2023-05-04 14:31:29 +0530

    second file is add by remote server

---
+++ Q1.java
@@ -0,0 +1,15 @@
+package dac23;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=0;i<=10;i++)
+		{
+			sum=sum+i;
+			System.out.println(""+(sum));
+			
+		}
+	}
+
+}
